d50abcacc8247d5ff7fa7c0f03ed478d35d65f9a
as14g18 <as14g18@soton.ac.uk>
2018-11-04 14:58:04 +0000

    Binded spirograph drawer to slider values

--- SliderBuilder.java
+++ SliderBuilder.java
@@ -1,13 +1,15 @@
 import javafx.scene.control.Slider;
 
 public class SliderBuilder {
-	public Slider buildSlider(int valueSetMin, int valueSetMax) {
+	public Slider buildSlider(int valueSetMin, int valueSetMax, int initialValue, boolean snapToTicks) {
 		Slider slider = new Slider();
-		slider.setValue(0);
+		slider.setValue(initialValue);
 		slider.setBlockIncrement(1); // increases value of slider by 1 when the user clicks on the slider line
-		
+		slider.setShowTickLabels(true);
+		slider.setShowTickMarks(true);
 		slider.setMin(valueSetMin);
 		slider.setMax(valueSetMax);
+		slider.setSnapToTicks(snapToTicks);
 		
 		return slider;
 	}
--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
@@ -12,7 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
 
@@ -34,12 +30,21 @@ public class UserInterface extends Application{
 		SliderBuilder sliderBuilder = new SliderBuilder(); // Factory class for building sliders in one line
 		LabelBuilder labelBuilder = new LabelBuilder(); // Label factory
 		
-		Slider sliderFixedRadius = sliderBuilder.buildSlider(0, 100); // variable R
-		Slider sliderMovingRadius = sliderBuilder.buildSlider(0, 100); // variable r
-		Slider sliderPenOffset = sliderBuilder.buildSlider(0, 100); // variable O
-		Label labelFixedRadius = labelBuilder.buildLabel("Fixed radius:");
-		Label labelMovingRadius = labelBuilder.buildLabel("Moving radius:");
-		Label labelPenOffset = labelBuilder.buildLabel("Pen offset:");
+		Slider sliderFixedRadius = sliderBuilder.buildSlider(0, 500, 250, false); // variable n
+		sliderFixedRadius.setValue(250);
+		Slider sliderMovingRadius = sliderBuilder.buildSlider(0, 500, 56, false); // variable m
+		Slider sliderPenOffset = sliderBuilder.buildSlider(0, 10, 5, false); // variable f
+		Slider sliderLoops = sliderBuilder.buildSlider(0, 150, 20, false);
+		Slider sliderScale = sliderBuilder.buildSlider(1, 20, 10, false);
+		Slider sliderXOffset = sliderBuilder.buildSlider(-500, 500, -100, false);
+		Slider sliderYOffset = sliderBuilder.buildSlider(-500, 500, 0, false);
+		Label labelFixedRadius = labelBuilder.buildLabel("  Fixed radius:");
+		Label labelMovingRadius = labelBuilder.buildLabel("  Moving radius:");
+		Label labelPenOffset = labelBuilder.buildLabel("  Pen offset:");
+		Label labelLoops = labelBuilder.buildLabel("  Number of loops:");
+		Label labelScale = labelBuilder.buildLabel("  Scale:");
+		Label labelXOffset = labelBuilder.buildLabel("  x Offset:");
+		Label labelYOffset = labelBuilder.buildLabel("  y Offset:");
 		Button buttonDrawGraph = new Button("Draw Spirograph");
 		
 		Canvas canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -51,7 +56,19 @@ public class UserInterface extends Application{
 	    GraphicsContext gc = canvas.getGraphicsContext2D(); // this is used to actually draw stuff
 	    
 		VBox leftMenu = new VBox(); // left aligned menu
-		leftMenu.getChildren().addAll(labelFixedRadius, sliderFixedRadius, labelMovingRadius, sliderMovingRadius, labelPenOffset, sliderPenOffset, buttonDrawGraph);
+		leftMenu.getChildren().addAll(labelFixedRadius, sliderFixedRadius, 
+				labelMovingRadius,sliderMovingRadius, 
+				labelPenOffset, sliderPenOffset, 
+				labelLoops, sliderLoops,
+				labelScale, sliderScale, 
+				labelXOffset, sliderXOffset, 
+				labelYOffset, sliderYOffset, 
+				buttonDrawGraph);
+		
+		buttonDrawGraph.setOnAction(e -> {
+			gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+			paint(gc, buttonDrawGraph, (int)sliderFixedRadius.getValue(), (int)sliderMovingRadius.getValue(), (int)sliderPenOffset.getValue(), (int)sliderLoops.getValue(), (int)sliderScale.getValue(), (int)sliderXOffset.getValue(), (int)sliderYOffset.getValue());
+		});
 		
 		BorderPane borderPane = new BorderPane();
 		borderPane.setLeft(leftMenu);
@@ -61,20 +78,18 @@ public class UserInterface extends Application{
 		Scene scene = new Scene(borderPane, WINDOW_WIDTH, WINDOW_HEIGHT);
 		window.setScene(scene);
 		window.show();
-		
-		paint(gc);
 	}
-	private void paint(GraphicsContext gc) {
-		// draws the spirograph
-		// Samples: 250/56
-		Hypocycloid hypocloid = new Hypocycloid(12344, 4321, 1, 1000, 100, -100, 0);
-		Paintbrush paintbrush = new Paintbrush(gc);
-		ArrayList<Tuple<Double, Double>> coordList = new ArrayList<Tuple<Double, Double>>();
-		coordList = hypocloid.getCoordList();
+	private void paint(GraphicsContext gc, Button buttonDrawGraph, int fixedRadius, int movingRadius, int penOffset, int loops, int scale, int xOffset, int yOffset) {
+		// paints the spirograph
+		// Hypocycloid hypocloid = new Hypocycloid(250, 56, 5, 1, 10, -100, 0);
+		Hypocycloid hypocycloid = new Hypocycloid(fixedRadius, movingRadius, penOffset, loops, scale, xOffset, yOffset);
 		
-		gc.setFill(Color.GREEN);
-		for (Tuple<Double, Double> coord: coordList) {
-			gc.fillOval(coord.getX() + WINDOW_WIDTH / 2, coord.getY() + WINDOW_HEIGHT / 2, 1, 1);
-		}
+		
+		ArrayList<Tuple<Double, Double>> coordList = new ArrayList<Tuple<Double, Double>>();
+		coordList = hypocycloid.getCoordList();
+		
+		Paintbrush paintbrush = new Paintbrush(gc, coordList, buttonDrawGraph, WINDOW_WIDTH, WINDOW_HEIGHT);
+		Runnable paintbrushThread = paintbrush;
+		new Thread(paintbrushThread).start();
 	}
 }
